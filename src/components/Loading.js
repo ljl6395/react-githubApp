f30505d5a6aa618290417f141e0140c579aa6d69
liujianli <376122478@qq.com>
2020-07-30 17:45:06 +0800

    Updates

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Loading extends Component {
-  render() {
-    return <div style={{ fontSize: 22, textAlign: "center" }}>Loading...</div>;
-  }
-}
-
-export default Loading;
